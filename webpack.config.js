0b748f357281f0cd89c9ccbb0f3847637a3d008a
Dmitrii Azarov <dmi-azarov@ya.ru>
2020-06-24 14:13:19 +0300

    исправлены мелкие ошибки

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
-const IS_DEV = NODE_ENV == 'development';
-const IS_PROD = NODE_ENV == 'production';
+const IS_DEV = NODE_ENV === 'development';
+const IS_PROD = NODE_ENV === 'production';
 
 function setupDevtool() {
     if (IS_DEV) return 'eval';
