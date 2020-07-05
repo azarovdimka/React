dceb722977d49b1f571e496e43a14f4912f51b29
Dmitrii Azarov <dmi-azarov@ya.ru>
2020-07-05 22:53:34 +0300

    Не запускается npm run build:prod

---
+++ cfg/webpack.client.config.js
@@ -0,0 +1,23 @@
+function setupDevtool() {
+    if (IS_DEV) return 'eval';
+    if (IS_PROD) return false;
+}
+
+module.exports = {
+    resolve: {
+        extensions: ['.jsx', '.js', '.json']
+    },
+    mode: NODE_ENV ? NODE_ENV : 'development',
+    entry: path.resolve(__dirname, '../src/client/index.jsx'),
+    output: {
+        path: path.resolve(__dirname, '../dist/client'),
+        filename: 'client.js'
+    },
+    module: {
+        rules: [{
+            test: /\.[tj]sx?$/,
+            use: ['ts-loader']
+        }]
+    },
+    devtool: setupDevtool()
+};
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Header } from "./header";
+import { Header } from "../shared/header";
 
 window.addEventListener('load', () => {
 
